09b1e4ac28fe0753ed4921f0a6632232cecd5844
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-12-07 18:56:28 +0530

    feat: implement S2-T1 – add User entity and repository with email uniqueness and BaseEntity inheritance

---
+++ src/main/java/org/chandra/dmabackend/model/User.java
@@ -0,0 +1,25 @@
+package org.chandra.dmabackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User extends BaseEntity{
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+}
