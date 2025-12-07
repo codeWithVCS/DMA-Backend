04ff43bf90d00fabae951cc4092bb1818fdfc1ff
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-12-07 17:01:00 +0530

    feat: implement S1-T1 – initialize Spring Boot backend project with core structure and dependencies

---
+++ src/main/java/org/chandra/dmabackend/DmaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.chandra.dmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmaBackendApplication.class, args);
+    }
+
+}
